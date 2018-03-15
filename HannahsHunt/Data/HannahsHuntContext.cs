15472186817cae28a5bc02f33a5ab862a22f9113
Stuart Hopwood <stoo.hopwood@gmail.com>
2018-03-15 22:46:12 +0000

    starting sqlite install

---
+++ HannahsHunt/Data/HannahsHuntContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace HannahsHunt.Models
+{
+    public class HannahsHuntContext : DbContext
+    {
+        public HannahsHuntContext (DbContextOptions<HannahsHuntContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HannahsHunt.Models.User> User { get; set; }
+    }
+}
