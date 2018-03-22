74383736a6db4eae5796c78eadb761154f67fa8f
Stuart Hopwood <stuart.hopwood@interactivets.com>
2018-03-22 14:23:41 +0000

    Styled Login Page

---
+++ HannahsHunt/Controllers/AdministrationController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using HannahsHunt.Models;
+using HannahsHunt.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace HannahsHunt.Controllers
+{
+    [Authorize(Roles="Administrator")]
+    public class AdministrationController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        //Create DB Context
+        private readonly ApplicationDbContext _context;
+
+        //Initialise Context
+        public AdministrationController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        //Clean Up Context
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        
+
+        public IActionResult Users()
+        {
+           List<ApplicationUser> users = _userManager.Users.ToList();
+           return View(users);
+        }
+    }
+}
