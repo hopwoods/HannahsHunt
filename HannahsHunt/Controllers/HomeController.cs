10c4fd1b1038a190b35ddbe43d4eb6af763e8f1d
Stuart Hopwood <stuart.hopwood@interactivets.com>
2018-03-26 18:33:44 +0100

    Adjusted font to larger size. Styled Forgotten password pages.

--- HannahsHunt/Controllers/HomeController.cs
+++ HannahsHunt/Controllers/HomeController.cs
@@ -9,28 +9,29 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace HannahsHunt.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
-        [Authorize]
+        
         public IActionResult Index()
         {
             return View();
         }
-        [Authorize(Roles = "Administrator")]
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
 
             return View();
         }
-        [AllowAnonymous]
+
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
 
             return View();
         }
-        [AllowAnonymous]
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
