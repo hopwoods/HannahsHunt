50e73038a21b96548e6e4a3a47a111f14e3404bd
Stuart Hopwood <stuart.hopwood@interactivets.com>
2018-06-14 11:24:26 +0100

    Update to Extensions for XML Documentation.

--- HannahsHunt/Extensions/Extensions.cs
+++ HannahsHunt/Extensions/Extensions.cs
@@ -11,33 +11,62 @@ namespace HannahsHunt.Extensions
 {
     public static class ClaimsPrincipalExtension
     {
+        /// <summary>
+        /// Method to retrieve the UserId ClaimsPrinciple value.
+        /// </summary>
+        /// <returns></returns>
         public static string GetUserId(this ClaimsPrincipal principal)
         {
             var userid = principal.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier");
             return userid?.Value;
         }
+
+        /// <summary>
+        /// Method to retrieve the FirstName ClaimsPrinciple value.
+        /// </summary>
+        /// <returns></returns>
         public static string GetFirstName(this ClaimsPrincipal principal)
         {
             var firstname = principal.Claims.FirstOrDefault(c => c.Type == "FirstName");
             return firstname?.Value;
         }
+
+        /// <summary>
+        /// Method to retrieve the LastName ClaimsPrinciple value.
+        /// </summary>
+        /// <returns></returns>
         public static string GetLastName(this ClaimsPrincipal principal)
         {
             var lastname = principal.Claims.FirstOrDefault(c => c.Type == "LastName");
             return lastname?.Value;
         }
+
+        /// <summary>
+        /// Method to retrieve the FullName ClaimsPrinciple value.
+        /// </summary>
+        /// <returns></returns>
         public static string GetFullName(this ClaimsPrincipal principal)
         {
             var fullName = principal.Claims.FirstOrDefault(c => c.Type == "FullName");
             return fullName?.Value;
         }
+
+        /// <summary>
+        /// Method to retrieve the value of a ClaimsPrinciple Claim
+        /// </summary>
+        /// <param name="claimType"></param>
+        /// <returns>Claim Value</returns>
         public static string GetUserClaim(this ClaimsPrincipal principal, string claimType)
         {
             var claim = principal.Claims.FirstOrDefault(c => c.Type == claimType);
             return claim?.Value;
         }
 
-        //Method for updating User Claims in the Database
+        /// <summary>Adds or Updates a User Claim to DB</summary>
+        /// <param name="userManager"> UserManager instance</param>
+        /// <param name="user"> ApplicationUser object </param>
+        /// <param name="claimType"> Claims Principle Claim Type</param>
+        /// <param name="value"> Claim Value</param>
         public static async Task AddUpdateClaimAsync(this ClaimsPrincipal principal, UserManager<ApplicationUser> userManager, ApplicationUser user, string claimType, string value)
         {
             UserManager<ApplicationUser> _userManager = userManager;
